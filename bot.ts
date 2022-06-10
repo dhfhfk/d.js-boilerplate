843c75e59d230c8a3afbc285fba296da628abbdd
dhfhfk <dhfhfk1203@gmail.com>
2022-06-10 21:25:36 +0900

    Localization 개선

--- bot.ts
+++ bot.ts
@@ -2,13 +2,12 @@ import { Client, Intents } from "discord.js";
 import config from "./config";
 
 const client = new Client({
-    intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILD_VOICE_STATES],
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 // 핸들러 불러오기
-["events", "slashCommands", "mongo"].forEach((handler) => {
+["events", "slashCommands", "mongo", "erela", "localization"].forEach((handler) => {
     try {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
         require(`./handlers/${handler}`)(client);
     } catch (e) {
         console.warn(e);
--- config.ts
+++ config.ts
@@ -3,9 +3,11 @@ import "dotenv/config";
 export interface Config {
     token: string;
     mongodb_uri: string;
+    languages: string[];
 }
 
 export default {
     token: process.env.TOKEN,
     mongodb_uri: process.env.MONGODB_URI,
+    languages: ["ko", "en-US"],
 } as Config;
